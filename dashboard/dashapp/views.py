521b519bb20bc3240ed23a423f6ccbd85f55b127
Kiran Kumar <kiran.c@itc.in>
2019-03-13 18:27:14 +0530

    s

--- dashboard/dashapp/views.py
+++ dashboard/dashapp/views.py
@@ -120,7 +120,7 @@ def list_links(request):
         lic = LIClient(tlclient)
         list_links = lic.list_links()
         template_data = {"list_links": list_links.get('message') } 
-        result = render(request, 'list_links.html', template_data)
+        result = render(request, 'home.html', template_data)
         #return HttpResponse(json.dumps(list_links))
         return result
 
