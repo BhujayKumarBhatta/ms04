bc542b4fa2f152a30971a095cbac7a4289dc47a7
Kiran Kumar <kiran.c@itc.in>
2019-03-18 15:13:46 +0530

    x

--- dashboard/dashapp/views.py
+++ dashboard/dashapp/views.py
@@ -149,9 +149,9 @@ def list_invoices(request):
     if request.method == 'GET': 
         tlclient = prep_tlclient_from_session(request)
         invClient = MSClient(tlclient) 
-        list_invoices = invClient.list_invoices()
+        list_invoices = invClient.list_invoices('all','all')
         #list_invoices = invClient.list()
-        template_data = {"list_invoices": list_invoices.get('all','all') } 
+        template_data = {"list_invoices": list_invoices.get('message') } 
         result = render(request, 'home.html', template_data)        
         return result
 
