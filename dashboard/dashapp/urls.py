d3644a118350df4bd81907a40b29c477c38985ee
Kiran Kumar <kiran.c@itc.in>
2019-03-19 11:44:56 +0530

    for File Upload from django.conf import settings from django.conf.urls.static import static

--- dashboard/dashapp/urls.py
+++ dashboard/dashapp/urls.py
@@ -13,6 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+#### For File Upload
+from django.conf import settings
+from django.conf.urls.static import static
+## End
 
 from django.urls import path
 from . import views
