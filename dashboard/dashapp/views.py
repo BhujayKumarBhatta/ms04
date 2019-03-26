5becc6882587ff29a2dd6a99f25d48defbfe74f0
Ubuntu <ubuntu@t4.itc.in>
2019-03-26 12:01:54 +0530

    views merge conflict

--- dashboard/dashapp/views.py
+++ dashboard/dashapp/views.py
@@ -54,19 +54,6 @@ def list_users(request):
     result = tlviews.list_users(request)
     return result
 
-<<<<<<< HEAD
-## End ****************************************************
-## Invoice Module ****************************************************
-def invoice(request):
-    if request.method == 'GET': 
-        #tlclient = prep_tlclient_from_session(request)
-        #list_users = tlclient.list_users()
-        template_data = {"INVOICE": "TRUE" }  
-        result = render(request, 'home.html', template_data)
-        #return HttpResponse(json.dumps(list_users))
-        return result
-=======
->>>>>>> 8d538000860fa0de07c1a2e4b1f3ddb5d70371ca
 
 def adduser(request):
     result = tlviews.adduser(request)
@@ -78,59 +65,6 @@ def list_invoices(request):
     return result
     
     
-<<<<<<< HEAD
-def list_links(request):
-    if request.method == 'GET': 
-        tlclient = prep_tlclient_from_session(request)
-        lic = LIClient(tlclient)
-        list_links = lic.list_links()
-        template_data = {"list_links": list_links.get('message') } 
-        result = render(request, 'home.html', template_data)
-        #return HttpResponse(json.dumps(list_links))
-        return result
-
-
-def list_test(request):
-    if request.method == 'GET': 
-        _param1 = request.GET['from']
-        _param2 = request.GET['name']
-        response = 'You are name is :' + _param1 + ' and from :' + _param2
-        return HttpResponse(response)   
-
-    
-### Manage Invoice Upload #####################
-## List All Invoice
-def list_invoices(request):
-    if request.method == 'GET': 
-        tlclient = prep_tlclient_from_session(request)
-        invClient = MSClient(tlclient) 
-        list_invoices = invClient.list_invoices('all','all')  
-        list_invoices = json.dumps(list_invoices)
-        new_jsoninvoices = json.loads(list_invoices)
-        
-        #list_invoices =
-        #JSON.parse(JSON.stringify(list_invoices).replace(/\s(?=\w+":)/g, ""))
-        
-
-        #new_json = {x.translate({32: None}): y for x, y in
-        #list_invoices.items()}
- 
-        #list_invoices = invClient.list()
-        template_data = None
-        template_data = {"list_invoices": new_jsoninvoices } 
-        result = render(request, 'home.html', template_data)        
-        return result
-
-## Navigate to Upload Invoice******
-def view_upload(request):
-   if request.method == 'GET':          
-        template_data = {"VIEW_UPLOAD": "TRUE" }  
-        result = render(request, 'home.html', template_data)        
-        return result
-
-## Upload Invoice******
-=======
->>>>>>> 8d538000860fa0de07c1a2e4b1f3ddb5d70371ca
 def invoice_upload(request):
     result = tspinv.invoice_upload(request)    
     return result
