ae5a218d0aa859c3fb9606a7c0f4e3598fef4528
Kiran Kumar <kiran.c@itc.in>
2019-03-27 17:39:28 +0530

    x

--- dashboard/dashapp/micros1/tspinv.py
+++ dashboard/dashapp/micros1/tspinv.py
@@ -111,9 +111,9 @@ def invoice_Update_upload(request):
         #loaded_message = json.loads(message)# Only gives json Object str
         
 
-        template_data = { "uploaded_file_url": uploaded_file_url,
-                              "VIEW_UPDATE_UPLOAD": "TRUE", 
-                              "UPLOAD_UPDATE_STATUS":message}
+        template_data = { 'uploaded_file_url': uploaded_file_url,
+                              'VIEW_UPDATE_UPLOAD': "TRUE", 
+                              'UPLOAD_UPDATE_STATUS':message}
         result = render(request, 'home.html',template_data)
         return result
     if request.method == 'GET':          
