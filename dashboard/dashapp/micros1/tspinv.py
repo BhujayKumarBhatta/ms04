fa00f377057371c04316b1697caf9d6eb5c33c91
Kiran Kumar <kiran.c@itc.in>
2019-03-27 17:38:05 +0530

    x

--- dashboard/dashapp/micros1/tspinv.py
+++ dashboard/dashapp/micros1/tspinv.py
@@ -114,7 +114,7 @@ def invoice_Update_upload(request):
         template_data = { "uploaded_file_url": uploaded_file_url,
                               "VIEW_UPDATE_UPLOAD": "TRUE", 
                               "UPLOAD_UPDATE_STATUS":message}
-        result = render(request, 'home.html',message)
+        result = render(request, 'home.html',template_data)
         return result
     if request.method == 'GET':          
         template_data = {"VIEW_UPDATE_UPLOAD": "from view upload" }  
