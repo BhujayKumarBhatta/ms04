7d61271e735d50c5b5113c7c15178df7715b3f2c
Kiran Kumar <kiran.c@itc.in>
2019-03-27 14:58:00 +0530

    x

--- dashboard/dashapp/micros1/tspinv.py
+++ dashboard/dashapp/micros1/tspinv.py
@@ -103,7 +103,7 @@ def view_update_upload(request):
 def invoice_Update_upload(request):
     if request.method == 'POST' and request.FILES['myfile']:
         myfile = request.FILES['myfile']
-        fs = FileSystemStorage()
+        fs = FileSystemStorage() 
         filename = fs.save(myfile.name, myfile)
         uploaded_file_url = fs.url(filename)
 
