365a2f06322702c43951c914b62edc28b7c39ae3
Kiran Kumar <kiran.c@itc.in>
2019-03-28 15:25:05 +0530

    x

--- dashboard/dashapp/urls.py
+++ dashboard/dashapp/urls.py
@@ -33,7 +33,7 @@ urlpatterns = [path('', views.login, name='login'),
     path('list_role', views.list_role, name='list_role'),
     path('list_ou', views.list_ou, name='list_ou'),
     #Invoice
-    path('list_invoices', views.list_invoices, name='invoice'),
+    path('list_invoices', views.list_invoices, name='list_invoices'),
     path('list_invoices_rcom', views.list_invoices_rcom, name='list_invoices_rcom'),    
     path('invoice_upload', views.invoice_upload, name='invoice_upload'),
     path('view_upload', views.view_upload, name='view_upload'),
