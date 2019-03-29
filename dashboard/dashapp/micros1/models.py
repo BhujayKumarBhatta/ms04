190a5bd0d1db5d687c3108f41eb9d9c75684cce1
Kiran Kumar <kiran.c@itc.in>
2019-03-29 11:27:54 +0530

    x

--- dashboard/dashapp/micros1/models.py
+++ dashboard/dashapp/micros1/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django import forms
+ 
 
 
 class Invoice(models.Model):
@@ -7,11 +7,11 @@ class Invoice(models.Model):
     InvoiceNnumber = models.CharField(max_length=500)
     CircuitID = models.IntegerField(default=0)
     Division = models.CharField(max_length=200)
-    TaxName = models.CharField(max_length=500) 
-    FullAddress = models.CharField(max_length=2000)
-    GSTNo = models.CharField(max_length=500)
+    TaxName  = models.CharField(max_length=500) 
+    FullAddress =  models.CharField(max_length=2000)
+    GSTNo =   models.CharField(max_length=500)
     State = models.CharField(max_length=500)
-    BillingDate_From = models.DateTimeField()
+    BillingDate_From  = models.DateTimeField()
     City = models.CharField(max_length=500)
     PremiseName = models.CharField(max_length=500)
     processing_status = models.CharField(max_length=500)
