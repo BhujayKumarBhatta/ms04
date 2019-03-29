03a950ffef3abd7f5ebb13b4d5cf6c9fe8924229
Kiran Kumar <kiran.c@itc.in>
2019-03-29 11:39:26 +0530

    x

--- dashboard/dashapp/micros1/models.py
+++ dashboard/dashapp/micros1/models.py
@@ -1,5 +1,5 @@
 from django.db import models
- 
+from django import forms
 
 
 class Invoice(models.Model):
@@ -7,7 +7,7 @@ class Invoice(models.Model):
     InvoiceNnumber = models.CharField(max_length=500)
     CircuitID = models.IntegerField(default=0)
     Division = models.CharField(max_length=200)
-    TaxName  = models.CharField(max_length=500) 
+    TaxName  = models.CharField(max_length=500)
     FullAddress =  models.CharField(max_length=2000)
     GSTNo =   models.CharField(max_length=500)
     State = models.CharField(max_length=500)
@@ -22,7 +22,11 @@ class Invoice(models.Model):
     Remarks = models.CharField(max_length=2000)
     InvoiceDate = models.DateTimeField()
     ServiceType = models.CharField(max_length=500)
-
+    message = forms.CharField(
+        max_length=2000,
+        widget=forms.Textarea(),
+        help_text='Write here your message!'
+    )
 
 
     def __unicode__(self):
