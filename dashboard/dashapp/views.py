6fc12cc22c674cd70dbb01b39be3d91c21ee75cd
Kiran Kumar <kiran.c@itc.in>
2019-03-29 15:29:29 +0530

    Form Validation

--- dashboard/dashapp/views.py
+++ dashboard/dashapp/views.py
@@ -97,38 +97,19 @@ def list_test(request):
 
 
 ######Invoice Model Form Testing
-
-#def add_model(request):
- 
-#    if request.method == "POST":
-#        form = invoiceForm(request.POST)
-#        if form.is_valid():
-#            model_instance = form.save(commit=False)
-#            model_instance.timestamp = timezone.now()
-#            model_instance.save()
-#            return redirect('/')
- 
-#    else:
- 
-#        form = invoiceForm() 
-#        return render(request, "invoicetemplate.html", {'form': form})
-
-
-
 def add_model(request):
-    # if this is a POST request we need to process the form data
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = NameForm(request.POST)
-        # check whether it's valid:
+ 
+    if request.method == "POST":
+        form = invoiceForm(request.POST)
         if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return HttpResponseRedirect('/thanks/')
+            model_instance = form.save(commit=False)
+            model_instance.timestamp = timezone.now()
+            model_instance.save()
+            return redirect('/') 
+    else: 
+        form = invoiceForm() 
+        return render(request, "invoicetemplate.html", {'form': form})
 
-    # if a GET (or any other method) we'll create a blank form
-    else:
-        form = NameForm()
 
-    return render(request, 'invoicetemplate.html', {'form': form})
+
+ 
