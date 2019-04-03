cbf54e17f3fe26eb59fc800734d80a430b147fb2
Kiran Kumar <kiran.c@itc.in>
2019-04-03 16:42:44 +0530

    x

--- dashboard/dashapp/micros1/tspinv.py
+++ dashboard/dashapp/micros1/tspinv.py
@@ -43,14 +43,13 @@ def list_invoices(request):
         template_data = {"list_invoices": list_invoices ,"ACCEPT_RCOM" : accept_recomondation} 
         result = render(request, 'home.html', template_data)        
         return result
-    if request.method == 'GET': 
-        respond_as_attachment(request)
-        #tlclient = tllogin.prep_tlclient_from_session(request)
-        #invClient = MSClient(tlclient) 
-        #list_invoices = invClient.list_invoices_clo('all','all')  
-        #template_data = {"list_invoices": list_invoices } 
-        #result = render(request, 'home.html', template_data)        
-        #return result
+    if request.method == 'GET':          
+        tlclient = tllogin.prep_tlclient_from_session(request)
+        invClient = MSClient(tlclient) 
+        list_invoices = invClient.list_invoices_clo('all','all')  
+        template_data = {"list_invoices": list_invoices } 
+        result = render(request, 'home.html', template_data)        
+        return result
 
 def list_invoice_rcom(request):
     if request.method == 'GET': 
@@ -203,33 +202,7 @@ def invoice_rcom_upload(request):
     return result
  
  
-def respond_as_attachment(request):
-    file_path =  "/home/ubuntu/dashboard/dashboard/sample-invoice-xl.xlsx"
-    original_filename = "sample-invoice-xl.xlsx"
-    fp = open(file_path, 'rb')
-    response = HttpResponse(fp.read())
-    fp.close()
-    type, encoding = mimetypes.guess_type(original_filename)
-    if type is None:
-        type = 'application/octet-stream'
-    response['Content-Type'] = type
-    response['Content-Length'] = str(os.stat(file_path).st_size)
-    if encoding is not None:
-        response['Content-Encoding'] = encoding
-
-    # To inspect details for the below code, see http://greenbytes.de/tech/tc2231/
-    if u'WebKit' in request.META['HTTP_USER_AGENT']:
-        # Safari 3.0 and Chrome 2.0 accepts UTF-8 encoded string directly.
-        filename_header = 'filename=%s' % original_filename.encode('utf-8')
-    elif u'MSIE' in request.META['HTTP_USER_AGENT']:
-        # IE does not support internationalized filename at all.
-        # It can only recognize internationalized URL, so we do the trick via routing rules.
-        filename_header = ''
-    else:
-        # For others like Firefox, we follow RFC2231 (encoding extension in HTTP headers).
-        filename_header = 'filename*=UTF-8\'\'%s' % urllib.quote(original_filename.encode('utf-8'))
-    response['Content-Disposition'] = 'attachment; ' + filename_header
-    return response
+ 
 # 
 # 
 # ## Upload Invoice******
