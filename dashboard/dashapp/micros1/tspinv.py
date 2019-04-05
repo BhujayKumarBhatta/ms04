ca91da684b84b7676bcafb9573b77e7653aa593c
Kiran Kumar <kiran.c@itc.in>
2019-04-05 15:35:24 +0530

    x

--- dashboard/dashapp/micros1/tspinv.py
+++ dashboard/dashapp/micros1/tspinv.py
@@ -88,9 +88,11 @@ def invoice_create(request):
             tlclient = tllogin.prep_tlclient_from_session(request)
             ms1Client = MSClient(tlclient)            
             extractedInvoice = extractInvoice(request)
+            #dictionary to 
+            #extractInvoice.INVOICE_OBJ.items()
             Newinvoice == json.dumps(extractedInvoice.INVOICE_OBJ)
             if extractedInvoice is not null:
-                create_result = ms1Client.create_invoice_list(extractedInvoice.INVOICE_OBJ)
+                create_result = ms1Client.create_invoice_list(extractInvoice.INVOICE_OBJ.items())
                 create_result_dump = json.dumps(create_result)
                 create_result_load = json.loads(message)
                 template_data = {"METHOD":METHOD, "VIEW_CREATE_INVOICE":
