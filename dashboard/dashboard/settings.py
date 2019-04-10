1a11777c6007cc73caeb0dd5fc6ea50b9a5426c7
Kiran Kumar <kiran.c@itc.in>
2019-04-10 16:09:41 +0530

    Session MAnagement Autologout start

--- dashboard/dashboard/settings.py
+++ dashboard/dashboard/settings.py
@@ -48,6 +48,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'Dasboard.DashboradMiddleware',
+
 ]
 
 ROOT_URLCONF = 'dashboard.urls'
