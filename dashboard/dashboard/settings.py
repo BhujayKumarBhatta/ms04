8200bcfbb3867d3ea2506bd730610b496def2739
Kiran Kumar <kiran.c@itc.in>
2019-04-10 16:39:42 +0530

    x

--- dashboard/dashboard/settings.py
+++ dashboard/dashboard/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
-    'dashapp.middleware.middleware.DashboradMiddleware',
+    'dashapp.middleware.DashboradMiddleware',
 
 ]
 
