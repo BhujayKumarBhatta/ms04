d1df93f56d4fb8e172edc4b68c86de5389da9607
Kiran Kumar <kiran.c@itc.in>
2019-04-10 16:14:25 +0530

    x

--- dashboard/dashboard/settings.py
+++ dashboard/dashboard/settings.py
@@ -48,9 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-    'DashboradMiddleware',
-
+     
 ]
 
 ROOT_URLCONF = 'dashboard.urls'
