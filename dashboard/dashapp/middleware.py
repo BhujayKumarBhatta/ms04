e5aa9cea7ae6e247b6749c16e4a287394d3c284e
Kiran Kumar <kiran.c@itc.in>
2019-04-11 15:27:27 +0530

    x

--- dashboard/dashapp/middleware.py
+++ dashboard/dashapp/middleware.py
@@ -5,7 +5,13 @@ from django.conf import settings
 from django.http import HttpResponsePermanentRedirect, HttpResponseRedirect
 #from django_common.session import SessionManager
 
-class DashboradMiddleware(object):
+try:
+    from django.utils.deprecation import MiddlewareMixin
+except ImportError:  # Django < 1.10
+    # Works perfectly for everyone using MIDDLEWARE_CLASSES
+    MiddlewareMixin = object
+
+class DashboradMiddleware(MiddlewareMixin):
     def process_exception(self, request, exception):
         print(exception.__class__.__name__)
         print(exception.message)
