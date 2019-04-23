ae9e135648749781db4fc46e2b47b18b473879ed
93431 <93431@ITSSBLRINFRL016.ITC.IN>
2019-04-23 16:34:35 +0530

    v

--- dashboard/dashapp/tokenleader/tlviews.py
+++ dashboard/dashapp/tokenleader/tlviews.py
@@ -92,19 +92,19 @@ def invoice_upload(request):
         		
 
 
-def org_delete(request):
+#def org_delete(request):
 #	try:
-    if request.method == 'POST':
-        tlclient = tllogin.prep_tlclient_from_session(request)
- 	orgname = request.POST['orgname']		 
-	data = dict({ "oname": ""})
-	data["oname"] = orgname 
-        status = tlclient.delete_org(data) 
-		list_org = tlclient.list_org()
+#    if request.method == 'POST':
+#        tlclient = tllogin.prep_tlclient_from_session(request)
+# 	orgname = request.POST['orgname']		 
+#	data = dict({ "oname": ""})
+#	data["oname"] = orgname 
+#        status = tlclient.delete_org(data) 
+#		list_org = tlclient.list_org()
 #        list_org = json.dumps(list_org)
-        template_data = {"list_org": list_org } 
-        result = render(request, 'home.html', template_data,"DELETE_STATUS":status)
-        return result  
+#        template_data = {"list_org": list_org } 
+#        result = render(request, 'home.html', template_data,"DELETE_STATUS":status)
+#        return result  
 #//	except Exception as exception:
 #//		 	tlclient = tllogin.prep_tlclient_from_session(request)
 #//		 	list_org = tlclient.list_org()
