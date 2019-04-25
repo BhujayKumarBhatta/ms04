c2bc1ae89661fdd5ebc5b3f00edf6c38bec73a47
93431 <93431@ITSSBLRINFRL016.ITC.IN>
2019-04-25 16:11:41 +0530

    v

--- dashboard/dashapp/tokenleader/tlviews.py
+++ dashboard/dashapp/tokenleader/tlviews.py
@@ -94,7 +94,7 @@ def delete_org(request):
         tlclient = tllogin.prep_tlclient_from_session(request)
         orgname = request.POST['orgname']
         data = dict({"orgname": ""})
-        data = {"oname": "orgname"}   
+        data = {"oname": "oname"}   
         status = tlclient.delete_org(data)
         list_org = tlclient.list_org()
         template_data = {"list_org": list_org }
