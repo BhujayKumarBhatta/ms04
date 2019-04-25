e71d03aafc75779549af032bbd3fc8b53b67375b
Kiran Kumar <kiran.c@itc.in>
2019-04-25 12:11:35 +0530

    Swagger

--- dashboard/dashboard/urls.py
+++ dashboard/dashboard/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
+from rest_framework_swagger.views import get_swagger_view
+
+
+schema_view = get_swagger_view(title='Polls API')
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('dashapp.urls')),
+    path(r'swagger-docs/', schema_view),
 ]
