39d69b45b1c18f76a3a7ed2ebed0a2743af947eb
prasen <prasen.biswas@itc.in>
2019-04-25 15:52:33 +0530

    indent

--- dashboard/dashapp/tokenleader/tlviews.py
+++ dashboard/dashapp/tokenleader/tlviews.py
@@ -90,14 +90,14 @@ def invoice_upload(request):
         return result
         		
 def delete_org(request):
-	if request.method == 'POST':
-	   tlclient = tllogin.prep_tlclient_from_session(request)
-	   orgname = request.POST['orgname']
-	   data = dict({"orgname": ""})
-	   data = {"oname": "orgname"}   
-       status = tlclient.delete_org(data)
-       list_org = tlclient.list_org()
-       template_data = {"list_org": list_org }
-       result = render(request, 'home.html', template_data)
-       return result
+    if request.method == 'POST':
+        tlclient = tllogin.prep_tlclient_from_session(request)
+        orgname = request.POST['orgname']
+        data = dict({"orgname": ""})
+        data = {"oname": "orgname"}   
+        status = tlclient.delete_org(data)
+        list_org = tlclient.list_org()
+        template_data = {"list_org": list_org }
+        result = render(request, 'home.html', template_data)
+        return result
 	
