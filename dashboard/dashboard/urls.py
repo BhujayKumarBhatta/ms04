6d3924e47a8fd83432ad9be74decd920cfb79cc8
Kiran Kumar <kiran.c@itc.in>
2019-04-25 12:43:58 +0530

    c

--- dashboard/dashboard/urls.py
+++ dashboard/dashboard/urls.py
@@ -18,7 +18,7 @@ from django.urls import path , include
 from rest_framework_swagger.views import get_swagger_view
 from rest_framework.documentation import include_docs_urls
 
-schema_view = get_swagger_view(title='Polls API')
+schema_view = get_swagger_view(title='TSP Billing')
 
 
 
@@ -26,5 +26,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('dashapp.urls')),
     path(r'swagger-docs/', schema_view),
-    path(r'docs/', include_docs_urls(title='Polls API')),
+    path(r'docs/', include_docs_urls(title='TSP Billing')),
 ]
