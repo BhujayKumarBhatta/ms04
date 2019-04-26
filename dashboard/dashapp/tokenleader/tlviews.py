9b6584ccd3940416362fa568d982295477f4b700
93431 <93431@ITSSBLRINFRL016.ITC.IN>
2019-04-26 19:55:56 +0530

    c

--- dashboard/dashapp/tokenleader/tlviews.py
+++ dashboard/dashapp/tokenleader/tlviews.py
@@ -35,7 +35,7 @@ def adduser(request):
         newuserdata["email"]= email
         newuserdata["wfc"]= wfc
         newuserdata["password"]= password
-        newuserdata.["roles"] = roles
+        newuserdata["roles"] = roles
         tlclient = tllogin.prep_tlclient_from_session(request)
         status = tlclient.add_user(newuserdata)
         list_users = tlclient.list_users()
