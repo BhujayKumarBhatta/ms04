a89d8e32f4b990cc814ca028564fab20940eebe5
93431 <93431@itsssmc001.ITC.IN>
2019-04-27 18:48:21 +0530

    f

--- dashboard/dashapp/tokenleader/tlviews.py
+++ dashboard/dashapp/tokenleader/tlviews.py
@@ -28,9 +28,9 @@ def adduser(request):
         username = request.POST.get('username')
         password = request.POST.get('password')		
         email = request.POST.get('email')
-        roles = request.POST.get{['roles']}
+        roles = request.POST.get(['roles'])
         wfc = request.POST.get('wfc')
-        newuserdata = dict({"username": "", "email": "", "password": "", "wfc": "", "roles": ""})	
+        newuserdata = dict({"username": "", "email": "", "password": "", "wfc": "", "roles": [""]})	
         newuserdata["username"]= username
         newuserdata["email"]= email
         newuserdata["wfc"]= wfc
