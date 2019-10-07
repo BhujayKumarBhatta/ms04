85b84cfffaa1da3b8d9a7cd35f6c30d7797c3060
93431 <93431@itsssmc001.ITC.IN>
2019-10-07 15:02:18 +0530

    f

--- dashboard/dashapp/tokenleader/tlviews.py
+++ dashboard/dashapp/tokenleader/tlviews.py
@@ -372,24 +372,10 @@ def add_service(request):
         template_data = {"list_services": list_services}
         template_name = 'admin_pages/list_service.html'
         web_page = validate_active_session(request, template_name, template_data)       
-        return web_page  
-        
-def delete_wfc(request):
-    if request.method == 'POST':
-        tlclient = tllogin.prep_tlclient_from_session(request)
-        wfcname = request.POST['wfcname']
-        data = dict({"wfcname": ""})
-        data["wfcname"] = wfcname 
-        #status = tlclient.delete_wfc(data)
-        status = tlclient.delete_wfc(wfcname)
-        list_wfc = tlclient.list_wfc()
-        template_data = {"delete_wfc": delete_wfc,"list_wfc": list_wfc,"DELETE_STATUS":status}
-        template_name = 'admin_pages/list_wfc.html'
-        web_page = validate_active_session(request, template_name, template_data)       
-        return web_page  
+        return web_page    
 
 def delete_service(request):
-    if request.method == 'GET': 
+    if request.method == 'POST': 
         tlclient = tllogin.prep_tlclient_from_session(request)
         servicename = request.POST['servicename']
         status = tlclient.delete_service(servicename)
